74a86d8807cb4bc1a611ced40073d863a3c487a2
Alexia E. Smith <washuu@gmail.com>
2016-06-26 15:01:19 -0500

    More documentation and bring over update/transmit.

--- CCD.cpp
+++ CCD.cpp
@@ -1,13 +1,28 @@
 #include "CCD.h"
+#include "CCDCodes.h"
 #include "Vehicles.h"
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include <Math.h>
 
+/*
+ * Constructor
+ *
+ * @access	public
+ * @param	object	HardwareSerial reference - Typically Serial1 on Arduino and Teensy
+ * @return	void
+ */
 CCD::CCD(HardwareSerial& serial) : ccdBus(serial) {
 	this->ccdBus.begin(this->ccdBaud);
 }
 
+/*
+ * Set new engine revolutions per minute.
+ *
+ * @access	public
+ * @param	float	RPM
+ * @return	void
+ */
 void CCD::setRPM(float rpm) {
 	int newRPM = round(constrain(rpm / 32, 0, 255));
 	if (newRPM != this->rpm) {
@@ -16,6 +31,13 @@ void CCD::setRPM(float rpm) {
 	}
 }
 
+/*
+ * Set new speed in miles per hour.
+ *
+ * @access	public
+ * @param	float	Speed in MPH
+ * @return	void
+ */
 void CCD::setMPH(float mph) {
 	//float kph = mph * 1.60934400061; //Update KPH at the same time for consistency.
 	int newMPH = round(constrain(mph * SPEED_MULTIPLIER, 0, 255));
@@ -25,8 +47,15 @@ void CCD::setMPH(float mph) {
 	}
 }
 
+/*
+ * Set new speed in kilometers per hour.
+ * Note: This functionality is incomplete due to needing a KPH cluster for testing.
+ *
+ * @access	public
+ * @param	float	Speed in KPH
+ * @return	void
+ */
 void CCD::setKPH(float kph) {
-	//Note: This functionality is incomplete due to needing a KPH cluster for testing.
 	//float mph = kph * 0.621371192; //Update MPH at the same time for consistency.
 	int newKPH = round(constrain(kph, 0, 255));
 	if (newKPH != this->kph) {
@@ -35,6 +64,13 @@ void CCD::setKPH(float kph) {
 	}
 }
 
+/*
+ * Set new oil pressure in pounds per square inch.
+ *
+ * @access	public
+ * @param	float	Oil PSI
+ * @return	void
+ */
 void CCD::setOilPSI(float oilPsi) {
 	int newPSI = round(constrain(oilPsi * 2, 0, 255));
 	if (newPSI != this->oilPsi) {
@@ -43,6 +79,13 @@ void CCD::setOilPSI(float oilPsi) {
 	}
 }
 
+/*
+ * Set new voltage.
+ *
+ * @access	public
+ * @param	float	Voltage
+ * @return	void
+ */
 void CCD::setVoltage(float voltage) {
 	int newVoltage = round(constrain(voltage * 8, 0, 255));
 	if (newVoltage != this->voltage) {
@@ -51,6 +94,13 @@ void CCD::setVoltage(float voltage) {
 	}
 }
 
+/*
+ * Set new coolant temperature
+ *
+ * @access	public
+ * @param	float	Temperature in fahrenheit.
+ * @return	void
+ */
 void CCD::setCoolantTemperature(float tempF) {
 	int hex = 0xC1; //193 - Also, fail safe to alerting in the red for temperature.
 	//If you are looking at this code for temperature ranges and thinking, "WTF", so am I.
@@ -71,6 +121,54 @@ void CCD::setCoolantTemperature(float tempF) {
 	}
 }
 
+/*
+ * Do updates to CDD bus.
+ *
+ * @access	public
+ * @return	boolean	Update performed.
+ */
 bool CCD::doUpdates() {
-	return true;
+	bool didUpdates = false;
+
+	//RPM has to be updated every tick, even with the engine off, to prevent the "no bus" error after twenty seconds.
+	this->busTransmit(RPM_MAP_ID, 2, this->rpm, 0x00); //Updating MAP not yet implemented.
+	this->needsUpdateRPM = false;
+	didUpdates = true;
+
+	//Speed also has to be updated within about three seconds or it falls back to zero.
+	this->busTransmit(SPEED_ID, 2, this->mph, this->kph);
+	this->needsUpdateSpeed = false;
+	didUpdates = true;
+
+	if (this->needsUpdateHealth) {
+		this->busTransmit(VOLTS_OILPSI_COOLTEMP_ID, 4, this->voltage, this->oilPsi, this->coolantTemperature, 0x00);
+		this->needsUpdateHealth = false;
+		didUpdates = true;
+	}
+
+	return didUpdates;
+}
+
+/*
+ * Do updates to CDD bus.
+ *
+ * @access	private
+ * @return	boolean	Update performed.
+ */
+void CCD::busTransmit(int id, int numBytes, ...) {
+	va_list bytes;
+	va_start(bytes, numBytes);
+
+	int checksum = id;
+	this->ccdBus.write(id);
+	for (int i = 0; i < numBytes; ++i) {
+		int toSend = va_arg(bytes, int);
+		this->ccdBus.write(toSend);
+		checksum += toSend;
+	}
+
+	va_end(bytes);
+
+	checksum = checksum & 0xFF;
+	this->ccdBus.write(checksum);
 }
--- CCD.h
+++ CCD.h
@@ -14,13 +14,14 @@ public:
 private:
 	const int ccdBaud = 7812; //Due to how AVRs work this ends up being the needed 7812.5 assuming the MHz has no remainder when divided by 1.
 	//Gauge values stored as a byte.
-	int rpm;
-	int mph;
-	int kph;
-	int oilPsi;
-	int voltage;
-	int coolantTemperature;
+	int rpm = 0x00;
+	int mph = 0x00;
+	int kph = 0x00;
+	int oilPsi = 0x00;
+	int voltage = 0x00;
+	int coolantTemperature = 0x00;
 	bool needsUpdateRPM = false;
 	bool needsUpdateSpeed = false;
 	bool needsUpdateHealth = false;
+	void busTransmit(int id, int numBytes, ...);
 };
