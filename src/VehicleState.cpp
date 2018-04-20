0cbe816775e5779fb94f4732051d0494f4b91503
John Grandle <john.p.grandle@gmail.com>
2018-04-19 22:34:18 -0700

    baselining vehicle status class

--- src/VehicleState.cpp
+++ src/VehicleState.cpp
@@ -1,5 +1,183 @@
 #include <VehicleState.h>
 
-int VehicleState::init(){
-  return 1;
+void VehicleState::init(){
+  this->volts = 0.0;
+  this->fuelLevel = 0.0;
+  this->tankCapacity = 30;
+  this->kph = 0.0;
+  this->mph = 0.0;
+  this->rpm = 0;
+  this->psi = 0.0;
+  this->deg_c = 0.0;
+  this->deg_f = 32.0;
+  this->pats_illuminated = false;
+  this->cruise_illuminated = false;
+  this->shift_illuminated = false;
+  this->cel_illuminated = false;
+  this->checkGauges_illuminated = false;
+  this->airbag_illuminated = false;
+  }
+
+float VehicleState::getVolts(){
+  return this->volts;
+}
+
+void VehicleState::setVolts(float volts){
+  this->volts=volts;
+}
+
+int VehicleState::getFuelLevel(){
+  return this->fuelLevel;
+}
+
+void VehicleState::setFuelLevel(int fuelLevel){
+  this->fuelLevel = fuelLevel;
+}
+
+float VehicleState::getFuelLeters(){
+  float actualPercentage = this->fuelLevel/100;
+  float gallons = tankCapacity * actualPercentage;
+  float leters = gallons * 3.79;
+  return leters;
+}
+
+void VehicleState::setSpeedMPH(float mph){
+   this->mph = mph;
+   this->kph = mph * 1.60934;
+ }
+float VehicleState::getSpeedMPH(){
+  return this->mph;
+}
+float VehicleState::getSpeedKPH(){
+  return this->kph;
+}
+
+void VehicleState::setRPMs(int rpm){
+  this->rpm = rpm;
+}
+
+int VehicleState::getRPMs(){
+  return this->rpm;
+}
+
+void VehicleState::setOilPSI(float psi){
+  this->psi = psi;
+}
+
+float VehicleState::getOilPSI(){
+  return this->psi;
+}
+
+void VehicleState::setTempF(float deg_f){
+  this->deg_f = deg_f;
+  this->deg_c = (deg_f - 32)*5/9;
+}
+
+void VehicleState::setTempC(float deg_c){
+  this->deg_c = deg_c;
+  this->deg_f = deg_c*9/5 + 32;
+}
+
+float VehicleState::getTempF(){
+  return this->deg_f;
+}
+
+float VehicleState::getTempC(){
+  return this->deg_c;
+}
+
+bool VehicleState::getPats(){
+  return this->pats_illuminated;
+}
+
+void VehicleState::setPats(bool pats_illuminated){
+  this->pats_illuminated = pats_illuminated;
+}
+
+void VehicleState::patsOn(){
+  this->setPats(true);
+}
+
+void VehicleState::patsOff(){
+  this->setPats(false);
+}
+
+bool VehicleState::getCruiseLamp(){
+  return this->cruise_illuminated;
+}
+
+void VehicleState::setCruiseLamp(bool status){
+  this->cruise_illuminated = status;
+}
+
+void VehicleState::cruiseLampOn(){
+  this->setCruiseLamp(true);
+}
+
+void VehicleState::cruiseLampOff(){
+  this->setCruiseLamp(false);
+}
+
+bool VehicleState::getShiftLamp(){
+  return this->shift_illuminated;
+}
+
+void VehicleState::setShiftLamp(bool status){
+  this->shift_illuminated = status;
+}
+
+void VehicleState::shiftLampOn(){
+  this->setShiftLamp(true);
+}
+
+void VehicleState::shiftLampOff(){
+  this->setShiftLamp(false);
+}
+
+bool VehicleState::getCel(){
+  return this->cel_illuminated;
+}
+
+void VehicleState::setCel(bool status){
+  this->cel_illuminated = status;
+}
+
+void VehicleState::celOn(){
+  this->setCel(true);
+}
+
+void VehicleState::celOff(){
+  this->setCel(false);
+}
+
+bool VehicleState::getCheckGauges(){
+  this->checkGauges_illuminated;
+}
+
+void VehicleState::setCheckGauges(bool state){
+  this->checkGauges_illuminated = state;
+}
+
+void VehicleState::checkGaugesOff(){
+  this->setCheckGauges(false);
+}
+
+void VehicleState::checkGaugesOn(){
+  this->setCheckGauges(true);
+}
+
+bool VehicleState::getAirbagLamp(){
+  return this->airbag_illuminated;
+}
+
+void VehicleState::setAirbagLamp(bool state){
+  this->airbag_illuminated = state;
+}
+
+void VehicleState::airbagLampOff(){
+  this->setAirbagLamp(false);
+}
+
+void VehicleState::airbagLampOn(){
+  this->setAirbagLamp(true);
 }
--- test/VehicleState_test.cpp
+++ test/VehicleState_test.cpp
@@ -1,20 +1,191 @@
 #ifdef UNIT_TEST
 #include "tests.h"
 #include <unity.h>
+#ifndef __VehicleState_h
+#include <VehicleState.h>
+#endif
 
 VehicleState* state;
 
 void setUpVehicleState(){
   state = new VehicleState();
+  state->init();
 }
 
 void VehicleState_tests_load(){
-  TEST_ASSERT_EQUAL_INT(1,state->init());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getVolts());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getFuelLevel());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getFuelLeters());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getSpeedKPH());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getSpeedMPH());
+  TEST_ASSERT_EQUAL_INT(0, state->getRPMs());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getOilPSI());
+  TEST_ASSERT_EQUAL_FLOAT(0.0, state->getTempC());
+  TEST_ASSERT_EQUAL_FLOAT(32.0, state->getTempF());
+  TEST_ASSERT_FALSE(state->getPats());
+  TEST_ASSERT_FALSE(state->getCruiseLamp());
+  TEST_ASSERT_FALSE(state->getShiftLamp());
+  TEST_ASSERT_FALSE(state->getCel());
+  TEST_ASSERT_FALSE(state->getCheckGauges());
+  TEST_ASSERT_FALSE(state->getAirbagLamp());
+}
+
+void can_manage_volts(){
+  state->setVolts(14.7);
+  TEST_ASSERT_EQUAL_FLOAT(14.7,state->getVolts());
+
+}
+
+void can_manage_fuel(){
+  state->setFuelLevel(100);
+  TEST_ASSERT_EQUAL_INT(100,state->getFuelLevel());
+
+  //the leters conversion is... rough.
+  TEST_ASSERT_EQUAL_FLOAT(113.7, state->getFuelLeters());
+  state->setFuelLevel(50);
+  TEST_ASSERT_EQUAL_FLOAT(56.85, state->getFuelLeters());
+}
+
+void can_manage_speed(){
+  state->setSpeedMPH(100);
+  TEST_ASSERT_EQUAL_FLOAT(100.0, state->getSpeedMPH());
+  TEST_ASSERT_EQUAL_FLOAT(160.934, state->getSpeedKPH());
+  state->setSpeedMPH(50);
+  TEST_ASSERT_EQUAL_FLOAT(50.0, state->getSpeedMPH());
+  TEST_ASSERT_EQUAL_FLOAT(80.4672, state->getSpeedKPH());
+
+}
+
+void can_manage_engine_speed(){
+state->setRPMs(100);
+TEST_ASSERT_EQUAL_INT(100, state->getRPMs());
+state->setRPMs(7000);
+TEST_ASSERT_EQUAL_INT(7000, state->getRPMs());
+}
+
+void can_manage_oil_pressure(){
+  state->setOilPSI(40.0);
+  TEST_ASSERT_EQUAL_FLOAT(40.0, state->getOilPSI());
+}
+
+void can_manage_engine_temperature(){
+state->setTempC(100.0);
+TEST_ASSERT_EQUAL_FLOAT(212.0, state->getTempF());
+TEST_ASSERT_EQUAL_FLOAT(100, state->getTempC());
+
+state->setTempC(0);
+TEST_ASSERT_EQUAL_FLOAT(32, state->getTempF());
+TEST_ASSERT_EQUAL_FLOAT(0, state->getTempC());
+
+state->setTempF(212.0);
+TEST_ASSERT_EQUAL_FLOAT(212.0, state->getTempF());
+TEST_ASSERT_EQUAL_FLOAT(100, state->getTempC());
+
+state->setTempF(32.0);
+TEST_ASSERT_EQUAL_FLOAT(32, state->getTempF());
+TEST_ASSERT_EQUAL_FLOAT(0, state->getTempC());
+}
+
+void can_manage_PATS(){
+  state->patsOn();
+  TEST_ASSERT_TRUE(state->getPats());
+
+  state->patsOff();
+  TEST_ASSERT_FALSE(state->getPats());
+
+  state->setPats(true);
+  TEST_ASSERT_TRUE(state->getPats());
+
+  state->setPats(false);
+  TEST_ASSERT_FALSE(state->getPats());
+}
+
+void can_manage_cruise_lamp(){
+  state->cruiseLampOn();
+  TEST_ASSERT_TRUE(state->getCruiseLamp());
+
+  state->cruiseLampOff();
+  TEST_ASSERT_FALSE(state->getCruiseLamp());
+
+  state->setCruiseLamp(true);
+  TEST_ASSERT_TRUE(state->getCruiseLamp());
+
+  state->setCruiseLamp(false);
+  TEST_ASSERT_FALSE(state->getCruiseLamp());
+}
+
+void can_manage_shift_lamp(){
+  state->shiftLampOn();
+  TEST_ASSERT_TRUE(state->getShiftLamp());
+
+  state->shiftLampOff();
+  TEST_ASSERT_FALSE(state->getShiftLamp());
+
+  state->setShiftLamp(true);
+  TEST_ASSERT_TRUE(state->getShiftLamp());
+
+  state->setShiftLamp(false);
+  TEST_ASSERT_FALSE(state->getShiftLamp());
+}
+
+void can_manage_CEL(){
+  state->celOn();
+  TEST_ASSERT_TRUE(state->getCel());
+
+  state->celOff();
+  TEST_ASSERT_FALSE(state->getCel());
+
+  state->setCel(true);
+  TEST_ASSERT_TRUE(state->getCel());
+
+  state->setCel(false);
+  TEST_ASSERT_FALSE(state->getCel());
+
+}
+
+void can_manage_checkGauges_lamp(){
+  state->checkGaugesOn();
+  TEST_ASSERT_TRUE(state->getCheckGauges());
+
+  state->checkGaugesOff();
+  TEST_ASSERT_FALSE(state->getCheckGauges());
+
+  state->setCheckGauges(true);
+  TEST_ASSERT_TRUE(state->getCheckGauges());
+
+  state->setCheckGauges(false);
+  TEST_ASSERT_FALSE(state->getCheckGauges())
+}
+
+void can_manage_airbag_lamp(){
+  state->airbagLampOn();
+  TEST_ASSERT_TRUE(state->getAirbagLamp());
+
+  state->airbagLampOff();
+  TEST_ASSERT_FALSE(state->getAirbagLamp());
+
+  state->setAirbagLamp(true);
+  TEST_ASSERT_TRUE(state->getAirbagLamp());
+
+  state->setAirbagLamp(false);
+  TEST_ASSERT_FALSE(state->getAirbagLamp());
 }
 
 int run_vehicleState_tests(){
     UNITY_BEGIN();
     RUN_TEST(VehicleState_tests_load);
+    RUN_TEST(can_manage_volts);
+    RUN_TEST(can_manage_fuel);
+    RUN_TEST(can_manage_speed);
+    RUN_TEST(can_manage_engine_speed);
+    RUN_TEST(can_manage_oil_pressure);
+    RUN_TEST(can_manage_engine_temperature);
+    RUN_TEST(can_manage_PATS);
+    RUN_TEST(can_manage_cruise_lamp);
+    RUN_TEST(can_manage_shift_lamp);
+    RUN_TEST(can_manage_CEL);
+    RUN_TEST(can_manage_checkGauges_lamp);
+    RUN_TEST(can_manage_airbag_lamp);
     return UNITY_END();
 }
 #endif
